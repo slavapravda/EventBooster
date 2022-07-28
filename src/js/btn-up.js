d1b60c9a9fc44bc72414b12382dd36adcb1c866f
Slava Pravda <pravdaslava7@gmail.com>
2022-07-28 19:30:19 +0300

    add up-btn in project

--- src/js/btn-up.js
+++ src/js/btn-up.js
@@ -1,17 +1,17 @@
-// window.addEventListener("scroll", scrollFunction);
-// const btnUpEl = document.querySelector('#upBtn')
+window.addEventListener('scroll', scrollFunction);
+const btnUpEl = document.querySelector('#upBtn');
 
-// btnUpEl.addEventListener('click', topFunction)
+btnUpEl.addEventListener('click', topFunction);
 
-// function scrollFunction() {
-//     if (window.pageYOffset > 150) {
-//         btnUpEl.style.display = "block";
-//     } else {
-//         btnUpEl.style.display = "none";
-//     }
-// }
+function scrollFunction() {
+  if (window.pageYOffset > 150) {
+    btnUpEl.style.display = 'block';
+  } else {
+    btnUpEl.style.display = 'none';
+  }
+}
 
-// function topFunction() {
-//     document.body.scrollTop = 0;
-//     document.documentElement.scrollTop = 0;
-// }
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
