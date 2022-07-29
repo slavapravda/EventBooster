c0e57c7e8b0c37aaf4fb8197041837b417ace1f9
Bohdan <102989300+Middasss@users.noreply.github.com>
2022-07-29 16:03:34 +0300

    delete clg

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -2,7 +2,7 @@ const preloaderMask = document.querySelector('.preloader');
 
 window.addEventListener('load', () => {
   preloaderMask.classList.add('hide');
-  console.log(preloaderMask);
+
   setTimeout(() => {
     preloaderMask.remove();
   }, 900);
