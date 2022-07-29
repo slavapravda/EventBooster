3f8c53ca8e1fa8a61085393f7a4566b463942c2a
Valeriia Siabruk <vsyabryk@gmail.com>
2022-07-29 15:12:54 +0300

    add modal functionality

--- src/js/open-modal.js
+++ src/js/open-modal.js
@@ -1,8 +1,11 @@
 import modal from '../templates/modal.hbs';
-import { fetchCardById } from './search-api';
+import { fetchCardById, fetchCardsByName } from './search-api';
+import cardsRender from '../templates/cards-render.hbs';
 
 const eventEl = document.querySelector('.event');
 const modalBackdropEl = document.querySelector('.backdrop');
+const conteinerEl = document.querySelector('.event .event__container');
+let currentAuthor;
 
 const openModal = event => {
   const eventCardEl = event.path.find(elem => {
@@ -10,7 +13,6 @@ const openModal = event => {
   });
 
   if (eventCardEl) {
-    console.log(eventCardEl.dataset.eventId);
     modalBackdropEl.classList.remove('is-hidden');
 
     fetchCardById(eventCardEl.dataset.eventId)
@@ -22,15 +24,19 @@ const openModal = event => {
           whenTime: `${result.dates.start.localTime.slice(
             0,
             -3
-          )} (${result.dates.timezone.replace('_', ' ')})`,
+          )} (${result.dates.timezone?.replace('_', ' ')})`,
           where: `${result._embedded.venues[0].city.name}, ${result._embedded.venues[0].country.name}`,
           whereName: result._embedded.venues[0].name,
-          who: result._embedded.attractions[0].name,
+          who: result._embedded.attractions
+            ? result._embedded.attractions[0].name
+            : '',
           prices: result.priceRanges?.map(({ max, min, type, currency }) => {
             return { title: `${type} ${min}-${max} ${currency}` };
           }),
+          image: result.images[0].url,
         };
 
+        currentAuthor = result._embedded.attractions[0].name;
         modalBackdropEl.innerHTML = modal(formatData);
       })
       .catch(error => console.log(error));
@@ -38,3 +44,32 @@ const openModal = event => {
 };
 
 eventEl.addEventListener('click', openModal);
+
+function closeModal() {
+  modalBackdropEl.innerHTML = '';
+  modalBackdropEl.classList.add('is-hidden');
+}
+
+modalBackdropEl.addEventListener('click', event => {
+  const isBackdrop = event.target.classList.contains('backdrop');
+  const isModalButtonClose = event.path.some(elem => {
+    return elem.classList?.contains('modal__btn-close');
+  });
+
+  if (isModalButtonClose || isBackdrop) {
+    closeModal();
+  }
+});
+
+modalBackdropEl.addEventListener('click', async event => {
+  const isMoreButton = event.path.some(elem => {
+    return elem.classList?.contains('more-btn');
+  });
+
+  if (isMoreButton) {
+    const response = await fetchCardsByName(currentAuthor, '');
+    const result = response.data._embedded.events;
+    conteinerEl.innerHTML = cardsRender(result);
+    closeModal();
+  }
+});
