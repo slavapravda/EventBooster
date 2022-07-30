11abe398d6fca097445a9667ae1f378c16b1fe18
Andrew Shkurenko <andrey.shkurenko@icloud.com>
2022-07-30 11:58:36 +0300

    Update main  (#71)
    * add project
    
    add project
    
    * add footer
    
    * add changes in footer
    
    * add styles for form
    
    * add button.html, add button.scss (update mixins for buttons)
    
    * зробив хедер
    
    добавив зміну теми
    
    * completed header and added theme color switcher, added 1 icon, and header pictures
    
    * add  logic for searchApi
    
    * add event card
    
    * add beat-heart in footer
    
    * fix/header
    
    * add common styles
    
    * fix common styles
    
    * add margin for event container
    
    * fix margin
    
    * add some changes
    
    * fix bags in form
    
    * bagfix
    
    fixed styles import, fixed script js
    
    * delete styles for select
    
    * Update index.html
    
    * clear form
    
    clear form
    
    * add bg for all page, rework theme functionality
    
    * add modal
    
    * add linear gradients to header pic
    
    * fix bags
    
    * add
    
    add
    
    * Fix bags in form
    
    * fix name of folder templade
    
    * add changes in modal
    
    * Fix bags in html and sprite
    
    * fix classes
    
    * changes
    
    * fix font styles
    
    * add variable in style
    
    * fixed bg, and other code
    
    * change modal
    
    * add redner for submit
    
    * add close btn and text color
    
    * fix bags in form
    
    * add some function
    
    * add transition on btn
    
    * add svg for location
    
    * Update form-search.js
    
    * fix bags
    
    * add render animation
    
    * add up btn
    
    * cards render
    
    * WIP: preloader
    
    * fixed bg for pc and tablet
    
    * add modal render
    
    * add styles for select option
    
    * WIP/preloader
    
    * fix cards grid
    
    * add pagination
    
    add pagination
    
    * add form-search to header
    
    * add notifications, upd form reset
    
    * completed preloader and added a few mixins
    
    * must be ok btn
    
    * Delete cards-render.hbs
    
    * fix package json
    
    * fix syntax error
    
    * add styles for select
    
    * save conflict
    
    * save files after gitt
    
    * add btn
    
    * change up btn
    
    * add
    
    * add cards render
    
    * add render on pagination click
    
    add render on pagination click
    
    * fix bug for add-render-onpafination
    
    fix bug for add-render-onpafination
    
    * add some styles for form
    
    * test push
    
    * lmdb-fixed
    
    * fix npm
    
    * new bg
    
    * fix function form-search
    
    * fixed bg
    
    * Add pagination
    
    * fix padding for a header title and add margins bottom
    
    * fix bg v2
    
    * add style for pagination
    
    * add modal functionality
    
    * add modal functionality
    
    * Update _pagination.scss
    
    * add resect select
    
    * fix bg v3
    
    * added transparent block in header & add position correction for card
    
    added transparent block in header & add position correction for card
    
    * delete clg
    
    * add styles for form elements
    
    * correct function for search country
    
    * add pagination for more from this author
    
    * Update form-search.js
    
    * add event location icon
    
    * add styles for animation
    
    * swap bg photos
    
    * up modal
    
    * fix pagination styles
    
    * fix: dublicates in styles
    
    * add styles for option block
    
    * customise header title
    
    * clear code
    
    * fix header and modal
    
    * add last version
    
    add last version
    
    * Create README.md
    
    * Update README.md
    
    * fix: add workflow
    
    Co-authored-by: Yuriy Dovzhyk <ydovzhyk@gmail.com>
    Co-authored-by: Slava Pravda <pravdaslava7@gmail.com>
    Co-authored-by: YDovzhyk <48929257+YDovzhyk@users.noreply.github.com>
    Co-authored-by: Kopchuk Maksym <kopchuk.maksym@gmail.com>
    Co-authored-by: fatime4 <102423500+fatime4@users.noreply.github.com>
    Co-authored-by: Bohdan <102989300+Middasss@users.noreply.github.com>
    Co-authored-by: Valeriia Siabruk <vsyabryk@gmail.com>
    Co-authored-by: Valeriia Siabruk <95078901+ValeriiaSiabruk@users.noreply.github.com>
    Co-authored-by: Andrew <vanillkay@gmail.com>

---
+++ src/js/open-modal.js
@@ -0,0 +1,90 @@
+import modal from '../templates/modal.hbs';
+import { fetchCardById, fetchCardsByName } from './search-api';
+import cardsRender from '../templates/cards-render.hbs';
+import { pageMenu } from './pagination';
+
+const eventEl = document.querySelector('.event');
+const modalBackdropEl = document.querySelector('.backdrop');
+const conteinerEl = document.querySelector('.event .event__container');
+let currentAuthor;
+
+const openModal = event => {
+  const eventCardEl = event.path.find(elem => {
+    return elem.classList?.contains('event__item');
+  });
+
+  if (eventCardEl) {
+    modalBackdropEl.classList.remove('is-hidden');
+    document.body.style.overflow = 'hidden';
+
+    fetchCardById(eventCardEl.dataset.eventId)
+      .then(response => {
+        const result = response.data._embedded.events[0];
+        const formatData = {
+          info: result.name,
+          when: result.dates.start.localDate,
+          whenTime: `${result.dates.start.localTime.slice(
+            0,
+            -3
+          )} (${result.dates.timezone?.replace('_', ' ')})`,
+          where: `${result._embedded.venues[0].city.name}, ${result._embedded.venues[0].country.name}`,
+          whereName: result._embedded.venues[0].name,
+          who: result._embedded.attractions
+            ? result._embedded.attractions[0].name
+            : '',
+          prices: result.priceRanges?.map(({ max, min, type, currency }) => {
+            return { title: `${type} ${min}-${max} ${currency}` };
+          }),
+          image: result.images[0].url,
+        };
+
+        currentAuthor = result._embedded.attractions[0].name;
+        modalBackdropEl.innerHTML = modal(formatData);
+      })
+      .catch(error => console.log(error));
+  }
+};
+
+eventEl.addEventListener('click', openModal);
+
+function closeModal() {
+  modalBackdropEl.innerHTML = '';
+  modalBackdropEl.classList.add('is-hidden');
+  document.body.style.overflow = 'scroll';
+}
+
+modalBackdropEl.addEventListener('click', event => {
+  const isBackdrop = event.target.classList.contains('backdrop');
+  const isModalButtonClose = event.path.some(elem => {
+    return elem.classList?.contains('modal__btn-close');
+  });
+
+  if (isModalButtonClose || isBackdrop) {
+    closeModal();
+  }
+});
+
+modalBackdropEl.addEventListener('click', async event => {
+  const isMoreButton = event.path.some(elem => {
+    return elem.classList?.contains('more-btn');
+  });
+
+  if (isMoreButton) {
+    const response = await fetchCardsByName(currentAuthor, '');
+    const result = response.data._embedded.events;
+    conteinerEl.innerHTML = cardsRender(result);
+    const pagination = pageMenu(response.data.page.totalElements);
+    pagination.on('beforeMove', async function (eventData) {
+      const page = eventData.page;
+      try {
+        const response = await fetchCardsByName(currentAuthor, '', page);
+        const result = response.data._embedded.events;
+        conteinerEl.innerHTML = cardsRender(result);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+    closeModal();
+  }
+});
